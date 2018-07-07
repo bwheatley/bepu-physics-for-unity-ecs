411ce962bad5b3ddbce88b1efc7b1e34bffc5c19
Eris Koleszar <eris.koleszar@gmail.com>
2018-07-07 16:29:23 -0500

    Intensify the number of entities being added and removed.

--- Assets/PhysicsTest.cs
+++ Assets/PhysicsTest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 
 public class PhysicsTest : MonoBehaviour
 {
+    private const int MAX_CHANGES_PER_FRAME = 100;
     private EntityManager _entityManager;
     private List<Entity> _entities = new List<Entity>();
 
@@ -21,16 +22,24 @@ public class PhysicsTest : MonoBehaviour
         var val = Random.value;
         if (val > 0.75f)
         {
-            var e = _entityManager.CreateEntity();
-            _entities.Add(e);
-            PhysicsSystem.Add(e);
+            var changes = Random.Range(0, MAX_CHANGES_PER_FRAME);
+            for (int i = 0; i < changes; i++)
+            {
+                var e = _entityManager.CreateEntity();
+                _entities.Add(e);
+                PhysicsSystem.Add(e);
+            }
         }
         else if (val < 0.25f && _entities.Count > 0)
         {
-            var i = Random.Range(0, _entities.Count);
-            var e = _entities[i];
-            PhysicsSystem.Remove(e);
-            _entities.RemoveAt(i);
+            var changes = Mathf.Min(_entities.Count, Random.Range(0, MAX_CHANGES_PER_FRAME));
+            for (int i = 0; i < changes; i++)
+            {
+                var index = Random.Range(0, _entities.Count);
+                var e = _entities[index];
+                PhysicsSystem.Remove(e);
+                _entities.RemoveAt(index);
+            }
         }
     }
 }
